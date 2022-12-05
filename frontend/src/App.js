c4f6384703ea4ea6f7c899273f2035383d9cf315
sniuraz <sam@example.com>
2022-12-05 15:59:43 +0200

    fe page not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,23 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Cart from './components/Cart';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 function App() {
-  return <div>
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='/cart' element={<Cart />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
-</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/not-found' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h2>404</h2>
+      <p>Toks puslapis nerastas</p>
+    </div>
+  )
+}
