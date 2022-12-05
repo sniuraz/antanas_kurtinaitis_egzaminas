855d859f754e27570a69a991162cfb264959886d
sniuraz <sam@example.com>
2022-12-05 16:48:45 +0200

    be products template

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    _id: '1',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop1',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '500',
+  },
+  {
+    _id: '2',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop2',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '600',
+  },
+  {
+    _id: '3',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop3',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '700',
+  },
+  {
+    _id: '4',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop4',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '800',
+  },
+  {
+    _id: '5',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop5',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '900',
+  },
+  {
+    _id: '6',
+    image:
+      'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=fe80&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+    name: 'laptop6',
+    brand: 'betkoks',
+    desc: 'Visai geras daiktas',
+    price: '1000',
+  },
+];
+
+module.exports = products;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const products = require('./data');
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,10 @@ app.get('/', (req, res) => {
   res.send('Sveiki užsukę į shopifis');
 });
 
-const port = process.env.PORT || 5000
+app.get('/products', (req, res) => {
+  res.send(products);
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server running on port ${port}`));
